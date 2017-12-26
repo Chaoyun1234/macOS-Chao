e8e91462e5e52b4b916766efd48b6e4da3af3af5
bys <bys@bysdembp-2.guest.corp.microsoft.com>
2017-12-26 16:47:59 +0800

    changeCrashes

--- macOS-Chao/ViewController.swift
+++ macOS-Chao/ViewController.swift
@@ -8,33 +8,23 @@
 
 import Cocoa
 import AppCenterCrashes
-import AppCenterAnalytics
+
 class ViewController: NSViewController {
-    
-    @IBOutlet weak var nameField: NSTextField!
-    
-    @IBOutlet weak var helloLabel: NSTextField!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Do any additional setup after loading the view.
     }
-    
+
     override var representedObject: Any? {
         didSet {
-            // Update the view, if already loaded.
+        // Update the view, if already loaded.
         }
     }
-    @IBAction func sayButtonClicked(_ sender: Any) {
-        var name = nameField.stringValue
-        if name.isEmpty{
-            name = "Word"
-            MSCrashes.generateTestCrash()
-            
-        }
-        let greeting = "Hello \(name)!"
-        helloLabel.stringValue = greeting
-        MSAnalytics.trackEvent("sayButtonClicked")
-    }
+
+   
+    
 }
+
